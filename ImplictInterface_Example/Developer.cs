c9582db8cda6268e2874d78e1322f6807588f55f
Dell <Dell@DESKTOP-13B42NJ>
2026-02-15 08:24:16 +0530

    Add project files.

---
+++ ImplictInterface_Example/Developer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplictInterface_Example
+{
+    internal class Developer:IEmployee
+    {
+        public void DisplayData()
+        {
+            Console.WriteLine("I am DispalyingData");
+        }
+
+        public  void Work()
+        {
+            Console.WriteLine("I am doing work");
+        }
+    }
+}
---
+++ ImplictInterface_Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplictInterface_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Developer devObj=new Developer();
+            devObj.DisplayData();
+            devObj.Work();
+            Console.ReadLine();
+        }
+    }
+}
